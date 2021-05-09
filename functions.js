dcedf4d485afdb1fc45822cc11f49dc07a45130b
Olga Sago <sago.olga@gmail.com>
2021-05-08 19:47:16 -0600

    Delete functions.js

--- functions.js
+++
@@ -1,7 +0,0 @@
-function myFunction(){var x=document.getElementById("navDemo");if(x.className.indexOf("w3-show")==-1){x.className+=" w3-show"}else{x.className=x.className.replace(" w3-show","")}}
-var modal=document.getElementById('ticketModal');window.onclick=function(event){if(event.target==modal){modal.style.display="none"}}
-var TxtRotate=function(el,toRotate,period){this.toRotate=toRotate;this.el=el;this.loopNum=0;this.period=parseInt(period,5)||1000;this.txt='';this.tick();this.isDeleting=!1};TxtRotate.prototype.tick=function(){var i=this.loopNum%this.toRotate.length;var fullTxt=this.toRotate[i];if(this.isDeleting){this.txt=fullTxt.substring(0,this.txt.length-1)}else{this.txt=fullTxt.substring(0,this.txt.length+1)}
-this.el.innerHTML='<span class="wrap">'+this.txt+'</span>';var that=this;var delta=160-Math.random()*100;if(this.isDeleting){delta/=4}
-if(!this.isDeleting&&this.txt===fullTxt){delta=this.period;this.isDeleting=!0}else if(this.isDeleting&&this.txt===''){this.isDeleting=!1;this.loopNum++;delta=900}
-setTimeout(function(){that.tick()},delta)};window.onload=function(){var elements=document.getElementsByClassName('txt-rotate');for(var i=0;i<elements.length;i++){var toRotate=elements[i].getAttribute('data-rotate');var period=elements[i].getAttribute('data-period');if(toRotate){new TxtRotate(elements[i],JSON.parse(toRotate),period)}}
-var css=document.createElement("style");css.type="text/css";css.innerHTML=".txt-rotate > .wrap { border-right: 0.08em solid #d184af }";document.body.appendChild(css)}
